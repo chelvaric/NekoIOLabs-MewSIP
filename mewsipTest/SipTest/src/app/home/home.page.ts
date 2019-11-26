091e04b4e1435a6c46284fbef4a1237e04c11180
ww <ww@qus.be>
2019-11-26 11:36:47 +0100

    full example

--- mewsipTest/SipTest/src/app/home/home.page.ts
+++ mewsipTest/SipTest/src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
-import { Simple, } from 'sip.js/lib/Web';
-import { InviteServerContext } from 'sip.js/lib/Session';
+import { Simple } from 'sip.js/lib/Web';
+import { InviteServerContext, InviteClientContext } from 'sip.js/lib/Session';
+import { UA } from 'sip.js/lib/UA';
+import { SessionDescriptionHandler } from 'sip.js/lib/session-description-handler';
 
 @Component({
   selector: 'app-home',
@@ -8,71 +10,82 @@ import { InviteServerContext } from 'sip.js/lib/Session';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements AfterViewInit {
-  
-  
+
+
   ngAfterViewInit(): void {
 
-    console.log(this.remote);
 
-    var options = {
-      media: {
-        local: {
-          video: this.local
-        },
-        remote: {
-          video: this.remote,
-          // This is necessary to do an audio/video call as opposed to just a video call
-          audio: this.remote
-        }
-      },
-      ua:{
-        uri: 'sip:chelvaric@sip.antisip.com',
-        authorizationUser: 'chelvaric',
-         transportOptions: {
-         wsServers: ['wss://sip.antisip.com']
-       },
-        password: 'sospiet2W'
-      }
-    };
 
-    this.client = new Simple(options);
- 
-    console.log(this.client);
 
-    this.client.on("ringing",this.recievingCall);
-    this.client.on("connected",()=>{ console.log("conected"); });
   }
 
 
-  @ViewChild('remoteVideo',{static: false}) remote: any;
-  @ViewChild('localVideo',{static: false}) local: any;
+  @ViewChild('remoteVideo', { static: false }) remote: any;
 
 
-  client: Simple;
+  userAgent: UA;
+
+  callSession: InviteClientContext;
+  remoteCallSession: InviteServerContext;
 
   constructor() {
 
-     
+    var options: UA.Options = {
+
+      uri: 'chelvaric@sip.antisip.com',
+      authorizationUser: 'chelvaric',
+      transportOptions: {
+        wsServers: ['wss://sip.antisip.com:9091']
+      },
+      password: 'sospiet2W'
+
+    };
+
+    this.userAgent = new UA(options)
+    this.userAgent.on('invite', this.recievingCall);
+    this.userAgent.on('registered',() => console.log('registered'));
+    this.userAgent.on('registrationFailed',() => console.log('failed to register'));
 
   }
 
 
 
 
-  recievingCall()
-  {
-     this.client.answer(); 
+  recievingCall(context: InviteServerContext) {
+    console.log("incomming call");
+    this.remoteCallSession = context;
+    context.accept();
+    context.on('trackAdded', this.AttachIncommingMedia);
   }
 
-  call()
-  {
-    this.client.call("sip:chelvaric2@sip.antisip.com")
+  //attach the video for an incomming call
+  AttachIncommingMedia() {
+    //get the peer Connection
+    var pc = this.remoteCallSession.sessionDescriptionHandler["peerConnection"];
+    //make the media stream
+    var remoteStream = new MediaStream();
+    //get audio and video track from remote
+    pc.getReceivers().forEach(function (receiver) {
+      remoteStream.addTrack(receiver.track);
+    });
+
+    //add the stream to the video element
+    this.remote.nativeElement["srcObject"] = remoteStream;
+    this.remote.nativeElement.play();
   }
 
-  hangUp()
-  {
-    this.client.hangup();
-  }
+ 
+
+ 
+
+  hangUp() {
+    if(this.remoteCallSession !== undefined)
+    {
+      this.callSession.terminate();
+      this.callSession = undefined;
+    }
   
+  }
+
 
 }
