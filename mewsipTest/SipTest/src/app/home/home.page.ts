36e68a9d80eab9be80ca4440955ca5ae464655d5
ww <ww@qus.be>
2020-01-22 16:40:27 +0100

    ready for test

--- mewsipTest/SipTest/src/app/home/home.page.ts
+++ mewsipTest/SipTest/src/app/home/home.page.ts
@@ -32,23 +32,31 @@ export class HomePage implements AfterViewInit {
 
     var options: UA.Options = {
 
-      uri: 'chelvaric@sip.antisip.com',
-      authorizationUser: 'chelvaric',
+      uri: '104@sip.qbus.be',
+      authorizationUser: '104',
+      displayName:'wouter',
       transportOptions: {
-        wsServers: ['wss://sip.antisip.com:9091']
+        wsServers: ['wss://sip.qbus.be:8089/ws']
       },
-      password: 'sospiet2W'
+      password: 'AABBCCDDEEFF'
 
     };
 
     this.userAgent = new UA(options)
-    this.userAgent.on('invite', this.recievingCall);
-    this.userAgent.on('registered',() => console.log('registered'));
-    this.userAgent.on('registrationFailed',() => console.log('failed to register'));
+    this.userAgent.on('invite', (session:InviteServerContext) => this.recievingCall(session));
+    this.userAgent.on('registered',(response?:any) => { console.log('registered'); console.log(response); this.userAgent.invite('10'); } );
+    this.userAgent.on('registrationFailed',(response?: any, cause?: any) => console.log('failed to register: ' + cause));
+    this.userAgent.on('inviteSent',(session:InviteClientContext) => { console.log('RECIEVED CALL'); console.log(session); })
 
   }
 
-
+  calling(context: InviteClientContext)
+  {
+     this.callSession = context;
+     context.on('failed',(reponse?:any,cause?:any) =>{ console.log("failed"); console.log(cause); });
+     context.on("accepted",(response: any,cause:any) => { console.log(cause);  console.log(response); })
+     context.on('trackAdded', this.AttachIncommingMedia);
+  }
 
 
   recievingCall(context: InviteServerContext) {
@@ -57,6 +65,24 @@ export class HomePage implements AfterViewInit {
     context.accept();
     context.on('trackAdded', this.AttachIncommingMedia);
   }
+
+   AttachOutgoingMedia()
+   {
+      //get the peer Connection
+    var pc = this.callSession.sessionDescriptionHandler["peerConnection"];
+    //make the media stream
+    var remoteStream = new MediaStream();
+    //get audio and video track from remote
+    pc.getReceivers().forEach(function (receiver) {
+      remoteStream.addTrack(receiver.track);
+    });
+
+    //add the stream to the video element
+    this.remote.nativeElement["srcObject"] = remoteStream;
+    this.remote.nativeElement.play();
+
+    console.log(this.remote.nativeElement);
+   }
 
   //attach the video for an incomming call
   AttachIncommingMedia() {
@@ -72,6 +98,8 @@ export class HomePage implements AfterViewInit {
     //add the stream to the video element
     this.remote.nativeElement["srcObject"] = remoteStream;
     this.remote.nativeElement.play();
+
+    console.log(this.remote.nativeElement);
   }
 
  
